589ee53079ced609b7e4529dbe766bf67d3e3df3
diamah06 <tabalmahdia@yahoo.fr>
2023-11-22 21:12:50 +0100

    Initial Commit

---
+++ Widget/WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetApp.swift
+//  Widget
+//
+//  Created by Mahdia Amriou on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
